9d42466a67d1ddf0d6ed263c75467c8f3f17e05a
Juan Molina <judegomolina@outlook.com>
2022-07-12 18:18:57 -0400

    update: adding inner class in inpunt port

--- backend/src/application/ports/input/create_tournament_port.py
+++ backend/src/application/ports/input/create_tournament_port.py
@@ -1,14 +1,6 @@
 from abc import ABC, abstractmethod
 
 
-class CreateTournamentCommand:
-
-        def __init__(self, tournament: Tournament):
-            self.tournament = tournament
-        
-        # TODO - Define the torunament object in the domain model
-
-
 class RegisterTournamentPort(ABC):
 
         class CreateTournamentCommand:
